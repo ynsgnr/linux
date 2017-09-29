caca57c4c4e105174fe8cb248db11124e06efb56
Yunus Güngör <ynsgnr@users.noreply.github.com>
2017-09-29 15:27:48 +0300

    Fİxed kernel crush bug
    Added more logging and error messages

--- set_myFlag/set_myFLag.c
+++ set_myFlag/set_myFLag.c
@@ -6,20 +6,36 @@
 
 asmlinkage long set_myFlag(pid_t pid, int flag){
 
+	printk("set my flag starts running with: %d, %d \n",pid,flag);
+
   struct task_struct *task;
   task=find_task_by_vpid(pid);
 
-  if(current_cred()->uid <= 0 || current_cred()->gid<=0){ //https://www.kernel.org/doc/Documentation/security/credentials.txt
-      //Process has root priv. set flag
-    	task->myFlag=flag;
-      printk('my_flag value of process %d has been set to %d',pid,flag);
-	//https://www.ibm.com/developerworks/library/l-kernel-logging-apis/index.html
-      return 0;
-  }
-  else{
-    //Not Root throw error
-    printk('my_flag value of process %d can not set to %d',pid,flag);
-	 //https://www.ibm.com/developerworks/library/l-kernel-logging-apis/index.html
-    return -EPERM; // http://man7.org/linux/man-pages/man2/syscalls.2.html
-  }
+	if (task>0)
+	{
+			printk("found task:  %d", task->pid);
+		if(current_cred()>0){
+			  if(current_cred()->uid <= 0 || current_cred()->gid<=0){ //https://www.kernel.org/doc/Documentation/security/credentials.txt
+			      //Process has root priv. set flag
+			     //task->myFlag=flag;
+			      printk("my_flag value of process %d has been set to %d \n",pid,flag);
+				//https://www.ibm.com/developerworks/library/l-kernel-logging-apis/index.html
+			      return 0;
+			  }
+			  else{
+			    //Not Root throw error
+			    printk("my_flag value of process %d can not set to %d \n",pid,flag);
+				 //https://www.ibm.com/developerworks/library/l-kernel-logging-apis/index.html
+			    return -EPERM; // http://man7.org/linux/man-pages/man2/syscalls.2.html
+			  }
+		}
+		else{
+			printk("user error \n");
+			return -EACCES;
+		}
+	}
+	else{
+		printk("no process found \n");
+		return -ESRCH;
+	}
 }
