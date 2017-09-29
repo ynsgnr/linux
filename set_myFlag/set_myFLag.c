b722006abb04df394cb95f1cb9662c770ba62e43
Yunus Güngör <ynsgnr@users.noreply.github.com>
2017-09-29 14:06:13 +0300

    Added myFlag to kernel and edited system call

--- set_myFlag/set_myFLag.c
+++ set_myFlag/set_myFLag.c
@@ -11,7 +11,7 @@ asmlinkage long set_myFlag(pid_t pid, int flag){
 
   if(current_cred()->uid <= 0 || current_cred()->gid<=0){ //https://www.kernel.org/doc/Documentation/security/credentials.txt
       //Process has root priv. set flag
-    // task->myFlag=flag;
+    	task->myFlag=flag;
       printk('my_flag value of process %d has been set to %d',pid,flag);
 	//https://www.ibm.com/developerworks/library/l-kernel-logging-apis/index.html
       return 0;
