eb6e6b441dd3692425c6f34b13872d5979d661b2
Yunus Güngör <ynsgnr@users.noreply.github.com>
2017-10-01 09:37:36 +0300

    Added testing set to 0

--- test_set_myValue.c
+++ test_set_myValue.c
@@ -4,6 +4,9 @@
 int main (void){
      int x1=10, x2=1,y;
       y=syscall(NR_mycall, x1, x2);
-	printf("%d",y);
+     printf("%d",y);
+     x2=0;
+      y=syscall(NR_mycall, x1, x2);
+     printf("%d",y);
      return 0 ;
 }
