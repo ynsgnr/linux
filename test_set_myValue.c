34504218d4fb4171554976b7f798ab7703db61bb
D. Cem Karagöz <dhncem@yandex.com>
2017-10-04 16:18:16 +0300

    changed the files to test do_exit

--- test_set_myValue.c
+++ test_set_myValue.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #define NR_mycall 355
+#include <unistd.h>
+
 
 int main (void){
      int x1=10, x2=1,y;
-      y=syscall(NR_mycall, x1, x2);
-     printf("%d",y);
-     x2=0;
-      y=syscall(NR_mycall, x1, x2);
-     printf("%d",y);
-     return 0 ;
+     y=syscall(NR_mycall, x1, x2);
+	 printf("Main MyFlag Value: %d\n",y);
+	 int pid;
+	
+	pid=fork();
+	
+	if(pid==0)
+	{
+		printf("I am the child\n");
+        printf("my pid=%d\n", getpid());
+			//while(1)
+			{
+				//empty must wait father to exit
+			}
+		return 0;
+	}
+	
+	else
+	{	
+		 
+		printf("I am the father\n");
+		printf("my pid=%d\n", getpid());
+		return 1;
+	}
+	
+	
+	
+	
+    
 }
